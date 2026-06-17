5ecc5114a34661518431ba06c2520cdc5d6822d5
Èric Bitriá Ribes <eric.bitria@gmail.com>
2026-06-17 14:29:20 +0200

    refactor: make Box a plain commonMain class
    Android's native Box wrapper was never read; all call sites round-trip
    through center/halfExtent. Collapse the expect + 4 actuals into one
    pure-Kotlin class.

--- kotlin/filament/src/commonMain/kotlin/io/github/erkko68/filament/Box.kt
+++ kotlin/filament/src/commonMain/kotlin/io/github/erkko68/filament/Box.kt
@@ -3,16 +3,29 @@ package io.github.erkko68.filament
 /**
  * An axis-aligned 3D box represented by its center and half-extent.
  */
-expect class Box {
-    constructor()
-    constructor(centerX: Float, centerY: Float, centerZ: Float, halfExtentX: Float, halfExtentY: Float, halfExtentZ: Float)
-    
-    val center: FloatArray
-    val halfExtent: FloatArray
-    
-    fun setCenter(x: Float, y: Float, z: Float)
-    fun setHalfExtent(x: Float, y: Float, z: Float)
+class Box(
+    val center: FloatArray = FloatArray(3),
+    val halfExtent: FloatArray = FloatArray(3)
+) {
+    constructor() : this(FloatArray(3), FloatArray(3))
 
-    val min: FloatArray
-    val max: FloatArray
+    constructor(centerX: Float, centerY: Float, centerZ: Float, halfExtentX: Float, halfExtentY: Float, halfExtentZ: Float) : this(
+        floatArrayOf(centerX, centerY, centerZ),
+        floatArrayOf(halfExtentX, halfExtentY, halfExtentZ)
+    )
+
+    fun setCenter(x: Float, y: Float, z: Float) {
+        center[0] = x
+        center[1] = y
+        center[2] = z
+    }
+
+    fun setHalfExtent(x: Float, y: Float, z: Float) {
+        halfExtent[0] = x
+        halfExtent[1] = y
+        halfExtent[2] = z
+    }
+
+    val min: FloatArray get() = floatArrayOf(center[0] - halfExtent[0], center[1] - halfExtent[1], center[2] - halfExtent[2])
+    val max: FloatArray get() = floatArrayOf(center[0] + halfExtent[0], center[1] + halfExtent[1], center[2] + halfExtent[2])
 }
